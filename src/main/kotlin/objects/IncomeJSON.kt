7b1193f196d4f7eb3185f3c959f6f1ccaf082a73
An Thi Nguyen <annguyen@Ans-MacBook-Pro.local>
2019-06-27 13:36:17 +0200

    Add conversion from String to YearMonth/Double

--- src/main/kotlin/objects/IncomeJSON.kt
+++ src/main/kotlin/objects/IncomeJSON.kt
@@ -1,5 +1,10 @@
 package objects
 
+import java.time.YearMonth
+import com.beust.klaxon.JsonValue
+import com.beust.klaxon.Converter
+import com.beust.klaxon.KlaxonException
+import java.time.format.DateTimeParseException
 
 //data class TotalInntekt(
 //        val inntektID: InntektID,
@@ -96,13 +101,13 @@ data class Inntekt (
 
         val arbeidsInntektMaaned : List<ArbeidsInntektMaaned>,
         val ident : Ident,
-        val fraDato : String,
-        val tilDato : String
+        @YearMonthDouble val fraDato : YearMonth,
+        @YearMonthDouble val tilDato : YearMonth
 )
 
 data class ArbeidsInntektMaaned (
 
-        val aarMaaned : String,
+        @YearMonthDouble val aarMaaned : YearMonth,
         val arbeidsInntektInformasjon : ArbeidsInntektInformasjon
 )
 
@@ -120,7 +125,7 @@ data class Opplysningspliktig (
 
 
 
-data class TotalInntekt (
+data class TotalInntekt @JvmOverloads constructor(
 
         val inntektId : InntektId,
         val inntekt : Inntekt,
@@ -138,13 +143,13 @@ data class InntektListe (
 
         val inntektType : String,
         val header : String,
-        val beloep : String,
+        @YearMonthDouble val beloep : Double,
         val fordel : String,
         val inntektskilde : String,
         val inntektsperiodetype : String,
         val inntektsstatus : String,
-        val leveringstidspunkt : String,
-        val utbetaltIMaaned : String,
+        @YearMonthDouble val leveringstidspunkt : YearMonth,
+        @YearMonthDouble val utbetaltIMaaned : YearMonth,
         val opplysningspliktig : Opplysningspliktig,
         val virksomhet : Virksomhet,
         val inntektsmottaker : Inntektsmottaker,
@@ -154,4 +159,29 @@ data class InntektListe (
         val verdikode : String,
         val beskrivelse : String
 )
+
+@Target(AnnotationTarget.FIELD)
+annotation class YearMonthDouble
+
+val klaxonConverter = object: Converter {
+    override fun canConvert(cls: Class<*>)
+            = cls == YearMonth::class.java
+            || cls == Double::class.java
+
+    override fun toJson(o: Any)
+    // TODO: Fix this
+            = """ { "date" : $o } """
+
+    override fun fromJson(jv: JsonValue): Any?
+            = try {
+        YearMonth.parse(jv.string)
+    } catch (e: DateTimeParseException) {
+        try {
+            jv.string?.toDouble()
+        }
+        catch (e: NumberFormatException) {
+            throw KlaxonException("Don't know how to convert ${jv.string}")
+        }
+    }
+}
 
--- src/test/kotlin/JSONParseTest.kt
+++ src/test/kotlin/JSONParseTest.kt
@@ -1,10 +1,15 @@
 import com.beust.klaxon.Klaxon
+import objects.YearMonthDouble
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.Assertions.assertTrue
 import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.Paths
 import objects.TotalInntekt
+import java.time.YearMonth
+import objects.klaxonConverter
+import java.io.File
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JSONParseTestClass{
@@ -12,10 +17,40 @@ class JSONParseTestClass{
     @Test
     fun JSONParsesTest () {
         println(Paths.get("").toAbsolutePath().toString())
-        val jsonFile = Files.newInputStream(Paths.get("src\\test\\resources\\ExpectedJSONResultForUserPeter"))
+        val jsonFile = Files.newInputStream(Paths.get("src/test/resources/ExpectedJSONResultForUserPeter"
+                .replace("/", File.separator)))
         val result = Klaxon()
+                .fieldConverter(YearMonthDouble::class, klaxonConverter)
                 .parse<TotalInntekt>(InputStreamReader(jsonFile))
         print(result)
+    }
+
+    @Test
+    fun JSONParsesToYearMonthTest () {
+        val jsonFile = Files.newInputStream(Paths.get("src/test/resources/ExpectedJSONResultForUserPeter"
+                .replace("/", File.separator)))
+        val result = Klaxon()
+                .fieldConverter(YearMonthDouble::class, klaxonConverter)
+                .parse<TotalInntekt>(InputStreamReader(jsonFile))
+
+        assertTrue(result!!.inntekt.fraDato == YearMonth.parse("2017-08"))
+        assertTrue(result.inntekt.tilDato == YearMonth.parse("2017-08"))
+        assertTrue(result.inntekt.arbeidsInntektMaaned[0].aarMaaned == YearMonth.parse("2017-08"))
+        assertTrue(result.inntekt.arbeidsInntektMaaned[0].arbeidsInntektInformasjon.inntektListe[0].leveringstidspunkt
+                == YearMonth.parse("2019-02"))
+        assertTrue(result.inntekt.arbeidsInntektMaaned[0].arbeidsInntektInformasjon.inntektListe[0].utbetaltIMaaned
+                == YearMonth.parse("2018-03"))
+    }
+
+    @Test
+    fun JSONParsesToDoubleTest () {
+        val jsonFile = Files.newInputStream(Paths.get("src/test/resources/ExpectedJSONResultForUserPeter"
+                .replace("/", File.separator)))
+        val result = Klaxon()
+                .fieldConverter(YearMonthDouble::class, klaxonConverter)
+                .parse<TotalInntekt>(InputStreamReader(jsonFile))
+
+        assertTrue(result!!.inntekt.arbeidsInntektMaaned[0].arbeidsInntektInformasjon.inntektListe[0].beloep == 5.83)
     }
 
 }
