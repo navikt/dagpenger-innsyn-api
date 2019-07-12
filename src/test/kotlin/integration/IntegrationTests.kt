14dcde7e04010b97a4ed006a4341a1bcc60a096d
Amund Bergland Kvalsvik <Amund.Bergland.Kvalsvik@nav.no>
2019-07-12 13:48:21 +0200

    Applied spotless

--- src/test/kotlin/integration/IntegrationTests.kt
+++ src/test/kotlin/integration/IntegrationTests.kt
@@ -1,16 +1,11 @@
 package integration
 
-import no.nav.dagpenger.innsyn.parsing.getJSONParsed
-import no.nav.dagpenger.innsyn.processing.convertInntektDataIntoProcessedRequest
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//class IntegrationTests {
+// @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+// class IntegrationTests {
 //
 //    // TODO: Add tests here that actually check that things work
 //    @Test
 //    fun canConvertInntektDataIntoProcessedRequest() {
 //        println(convertInntektDataIntoProcessedRequest(getJSONParsed("Gabriel")))
 //    }
-//}
+// }
