9ee405938ee051e69061b1f0af0e34e2e67be2fc
Giao The Cung <giaothe@gmail.com>
2019-07-17 12:15:40 +0200

    Change to preprod.local kafka cluster.

--- src/main/kotlin/no/nav/dagpenger/innsyn/data/configuration/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/innsyn/data/configuration/Configuration.kt
@@ -10,54 +10,54 @@ import com.natpryce.konfig.stringType
 import no.nav.dagpenger.streams.KafkaCredential
 
 private val localProperties = ConfigurationMap(
-        mapOf(
-                "vault.mountpath" to "postgresql/dev/",
-                "srvdp.inntekt.innsyn.username" to "igroup",
-                "srvdp.inntekt.innsyn.password" to "itest",
-                "enhetsregisteret.url" to "https://data.brreg.no/enhetsregisteret/api/enheter/",
-                "aktoerregisteret.url" to "http://backend.myapp.com:9011",
-                "oppslag.url" to "https://localhost:8090/api",
-                "oidc.sts.issuerurl" to "http://localhost/",
-                "kafka.bootstrap.servers" to "localhost:9092",
-                "jwks.url" to "http://host.docker.internal:4352/certs",
-                "jwks.issuer" to "http://simple-oidc-provider",
-                "application.profile" to "LOCAL",
-                "application.url" to "/inntekt",
-                "application.httpPort" to "8099",
-                "SRVDP.INNTEKT.INNSYN.USERNAME" to "igroup",
-                "srvdp.inntekt.innsyn.password" to "itest"
-        )
+    mapOf(
+        "vault.mountpath" to "postgresql/dev/",
+        "srvdp.inntekt.innsyn.username" to "igroup",
+        "srvdp.inntekt.innsyn.password" to "itest",
+        "enhetsregisteret.url" to "https://data.brreg.no/enhetsregisteret/api/enheter/",
+        "aktoerregisteret.url" to "http://backend.myapp.com:9011",
+        "oppslag.url" to "https://localhost:8090/api",
+        "oidc.sts.issuerurl" to "http://localhost/",
+        "kafka.bootstrap.servers" to "localhost:9092",
+        "jwks.url" to "http://host.docker.internal:4352/certs",
+        "jwks.issuer" to "http://simple-oidc-provider",
+        "application.profile" to "LOCAL",
+        "application.url" to "/inntekt",
+        "application.httpPort" to "8099",
+        "SRVDP.INNTEKT.INNSYN.USERNAME" to "igroup",
+        "srvdp.inntekt.innsyn.password" to "itest"
+    )
 )
 
 private val devProperties = ConfigurationMap(
-        mapOf(
-                "vault.mountpath" to "postgresql/preprod-fss/",
-                "enhetsregisteret.url" to "https://no.nav.dagpenger.innsyn.data.brreg.no/enhetsregisteret/api/enheter/",
-                "aktoerregisteret.url" to "http://tjenester.nav.no/aktoerregister/api/v1/identer?identgruppe=AktoerId",
-                "oppslag.url" to "http://dagpenger-oppslag.default.svc.nais.local/api",
-                "oidc.sts.issuerurl" to "https://security-token-service-t4.nais.preprod.local/",
-                "kafka.bootstrap.servers" to "d26apvl00159.test.local:8443,d26apvl00160.test.local:8443,d26apvl00161.test.local:8443",
-                "jwks.url" to "https://isso-q.adeo.no:443/isso/oauth2/connect/jwk_uri",
-                "jwks.issuer" to "https://isso-q.adeo.no:443/isso/oauth2",
-                "application.profile" to "DEV",
-                "application.url" to "/inntekt",
-                "application.httpPort" to "8099"
-        )
+    mapOf(
+        "vault.mountpath" to "postgresql/preprod-fss/",
+        "enhetsregisteret.url" to "https://no.nav.dagpenger.innsyn.data.brreg.no/enhetsregisteret/api/enheter/",
+        "aktoerregisteret.url" to "http://tjenester.nav.no/aktoerregister/api/v1/identer?identgruppe=AktoerId",
+        "oppslag.url" to "http://dagpenger-oppslag.default.svc.nais.local/api",
+        "oidc.sts.issuerurl" to "https://security-token-service-t4.nais.preprod.local/",
+        "kafka.bootstrap.servers" to "b27apvl00045.preprod.local:8443,b27apvl00046.preprod.local:8443,b27apvl00047.preprod.local:8443",
+        "jwks.url" to "https://isso-q.adeo.no:443/isso/oauth2/connect/jwk_uri",
+        "jwks.issuer" to "https://isso-q.adeo.no:443/isso/oauth2",
+        "application.profile" to "DEV",
+        "application.url" to "/inntekt",
+        "application.httpPort" to "8099"
+    )
 )
 private val prodProperties = ConfigurationMap(
-        mapOf(
-                "vault.mountpath" to "postgresql/prod-fss/",
-                "enhetsregisteret.url" to "https://no.nav.dagpenger.innsyn.data.brreg.no/enhetsregisteret/api/enheter/",
-                "aktoerregisteret.url" to "http://tjenester.nav.no/aktoerregister/api/v1/identer?identgruppe=AktoerId",
-                "oppslag.url" to "http://dagpenger-oppslag.default.svc.nais.local/api",
-                "oidc.sts.issuerurl" to "https://security-token-service.nais.adeo.no/",
-                "kafka.bootstrap.servers" to "a01apvl00145.adeo.no:8443,a01apvl00146.adeo.no:8443,a01apvl00147.adeo.no:8443,a01apvl00148.adeo.no:8443,a01apvl00149.adeo.no:8443,a01apvl150.adeo.no:8443",
-                "jwks.url" to "https://isso.adeo.no:443/isso/oauth2/connect/jwk_uri",
-                "jwks.issuer" to "https://isso.adeo.no:443/isso/oauth2",
-                "application.profile" to "PROD",
-                "application.url" to "/inntekt",
-                "application.httpPort" to "8099"
-        )
+    mapOf(
+        "vault.mountpath" to "postgresql/prod-fss/",
+        "enhetsregisteret.url" to "https://no.nav.dagpenger.innsyn.data.brreg.no/enhetsregisteret/api/enheter/",
+        "aktoerregisteret.url" to "http://tjenester.nav.no/aktoerregister/api/v1/identer?identgruppe=AktoerId",
+        "oppslag.url" to "http://dagpenger-oppslag.default.svc.nais.local/api",
+        "oidc.sts.issuerurl" to "https://security-token-service.nais.adeo.no/",
+        "kafka.bootstrap.servers" to "a01apvl00145.adeo.no:8443,a01apvl00146.adeo.no:8443,a01apvl00147.adeo.no:8443,a01apvl00148.adeo.no:8443,a01apvl00149.adeo.no:8443,a01apvl150.adeo.no:8443",
+        "jwks.url" to "https://isso.adeo.no:443/isso/oauth2/connect/jwk_uri",
+        "jwks.issuer" to "https://isso.adeo.no:443/isso/oauth2",
+        "application.profile" to "PROD",
+        "application.url" to "/inntekt",
+        "application.httpPort" to "8099"
+    )
 )
 
 data class Configuration(
@@ -102,7 +102,7 @@ enum class Profile {
 }
 
 fun config() = when (System.getenv("NAIS_CLUSTER_NAME")
-        ?: System.getProperty("NAIS_CLUSTER_NAME")) {
+    ?: System.getProperty("NAIS_CLUSTER_NAME")) {
     "dev-sbs" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding devProperties
     "prod-sbs" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding prodProperties
     "dev-fss" -> ConfigurationProperties.systemProperties() overriding EnvironmentVariables overriding devProperties
