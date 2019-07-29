62183b90e3582ad8a66c42d8959aa578ab090a18
An Thi Nguyen <annguyen@Ans-MacBook-Pro.local>
2019-07-29 16:58:16 +0200

    Add tests for naisChecks. Refactor routing

---
+++ src/main/kotlin/no/nav/dagpenger/innsyn/routing/InntektRoute.kt
@@ -0,0 +1,77 @@
+package no.nav.dagpenger.innsyn.routing
+
+import com.auth0.jwt.exceptions.JWTDecodeException
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.auth.authenticate
+import io.ktor.auth.authentication
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.util.pipeline.PipelineContext
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withTimeout
+import mu.KLogger
+import mu.KotlinLogging
+import no.nav.dagpenger.events.moshiInstance
+import no.nav.dagpenger.innsyn.conversion.convertInntektDataIntoUserInformation
+import no.nav.dagpenger.innsyn.conversion.objects.UserInformation
+import no.nav.dagpenger.innsyn.lookup.InnsynProducer
+import no.nav.dagpenger.innsyn.lookup.getGjeldendeAktoerIDFromIDToken
+import no.nav.dagpenger.innsyn.lookup.objects.Behov
+import no.nav.dagpenger.innsyn.lookup.objects.PacketStore
+import no.nav.dagpenger.innsyn.settings.Configuration
+import no.nav.dagpenger.innsyn.testDataSpesifisertInntekt
+import java.time.LocalDate
+
+private val logger: KLogger = KotlinLogging.logger {}
+private val config = Configuration()
+
+internal fun Routing.behov(packetStore: PacketStore, kafkaProducer: InnsynProducer) {
+    authenticate("jwt") {
+        get(config.application.applicationUrl) {
+            val idToken = call.request.cookies["ID_token"]
+            val beregningsdato = LocalDate.now()
+            if (idToken == null) {
+                logger.error("Received invalid request without ID_token cookie", call)
+                call.respond(HttpStatusCode.NotAcceptable, "Missing required cookies")
+            } else {
+                val aktoerID = getGjeldendeAktoerIDFromIDToken(idToken, getSubject())
+                try {
+                    mapRequestToBehov(aktoerID, beregningsdato).apply {
+                        kafkaProducer.produceEvent(this)
+                    }.also {
+                        withTimeout(30000) {
+                            while (!(packetStore.isDone(it.behovId))) {
+                                delay(2000)
+                            }
+                        }
+                        call.respond(HttpStatusCode.OK, moshiInstance.adapter(UserInformation::class.java).toJson(convertInntektDataIntoUserInformation(testDataSpesifisertInntekt)))
+                    }
+                } catch (e: TimeoutCancellationException) {
+                    logger.error("Timed out waiting for kafka", e)
+                    call.respond(HttpStatusCode.GatewayTimeout, moshiInstance.adapter(UserInformation::class.java).toJson(convertInntektDataIntoUserInformation(testDataSpesifisertInntekt)))
+                }
+            }
+        }
+    }
+}
+
+private fun PipelineContext<Unit, ApplicationCall>.getSubject(): String {
+    return runCatching {
+        call.authentication.principal?.let {
+            (it as JWTPrincipal).payload.subject
+        } ?: throw JWTDecodeException("Unable to get subject from JWT")
+    }.getOrElse {
+        logger.error(it) { "Unable to get subject from authentication" }
+        return@getOrElse "UNKNOWN"
+    }
+}
+
+internal fun mapRequestToBehov(aktorId: String, beregningsDato: LocalDate): Behov = Behov(
+        aktørId = aktorId,
+        beregningsDato = beregningsDato
+)
