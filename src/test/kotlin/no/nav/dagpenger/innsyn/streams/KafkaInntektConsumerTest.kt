7737a3f94006cb40a4176f09fb21f50e35ec1293
An Thi Nguyen <annguyen@Ans-MacBook-Pro.local>
2019-07-29 08:49:23 +0200

    Add kafka consumer tests

---
+++ src/test/kotlin/no/nav/dagpenger/innsyn/streams/KafkaInntektConsumerTest.kt
@@ -0,0 +1,73 @@
+package no.nav.dagpenger.innsyn.streams
+
+import io.mockk.Called
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verifyAll
+import no.nav.dagpenger.events.Packet
+import no.nav.dagpenger.innsyn.lookup.InntektPond
+import no.nav.dagpenger.innsyn.lookup.objects.PacketStore
+import no.nav.dagpenger.innsyn.settings.PacketKeys
+import no.nav.dagpenger.innsyn.settings.behovId
+import no.nav.dagpenger.streams.Topics
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.TopologyTestDriver
+import org.apache.kafka.streams.test.ConsumerRecordFactory
+import org.junit.jupiter.api.Test
+import java.util.Properties
+
+internal class KafkaInntektConsumerTest {
+
+    @Test
+    fun `Packet is ignored if not all parameters are present`() {
+        val packet = Packet().apply { this.putValue(PacketKeys.INNTEKT, "inntekt") }
+        val mock = mockk<PacketStore>()
+        runTest(mock, packet) {
+            verifyAll { mock wasNot Called }
+        }
+    }
+
+    @Test
+    fun `Packet is handled if all parameters are present`() {
+        val packet = Packet("""
+            {
+                ${PacketKeys.BEHOV_ID}: "behovId",
+                ${PacketKeys.INNTEKT}: "inntekt",
+                ${PacketKeys.MINSTEINNTEKT_RESULTAT}: "minsteinntektResultat",
+                ${PacketKeys.MINSTEINNTEKT_INNTEKTSPERIODER}: "minsteinntektInntektsperioder",
+                ${PacketKeys.PERIODE_RESULTAT}: "periodeResultat"
+            }
+        """.trimIndent()
+        )
+        val mock = mockk<PacketStore>().apply {
+            every { this@apply.insert(match { it.behovId == "behovId" }) } just Runs
+        }
+        runTest(mock, packet) {
+            verifyAll { mock.insert(match { it.behovId == "behovId" }) }
+        }
+    }
+
+    private companion object {
+        val factory = ConsumerRecordFactory<String, Packet>(
+                Topics.DAGPENGER_BEHOV_PACKET_EVENT.name,
+                Topics.DAGPENGER_BEHOV_PACKET_EVENT.keySerde.serializer(),
+                Topics.DAGPENGER_BEHOV_PACKET_EVENT.valueSerde.serializer()
+        )
+
+        val config = Properties().apply {
+            this[StreamsConfig.APPLICATION_ID_CONFIG] = "test"
+            this[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = "dummy:1234"
+        }
+
+        fun runTest(store: PacketStore, packet: Packet, testBlock: () -> Unit) {
+            InntektPond(store).let {
+                TopologyTestDriver(it.buildTopology(), config).use { topologyTestDriver ->
+                    topologyTestDriver.pipeInput(factory.create(packet))
+                    testBlock()
+                }
+            }
+        }
+    }
+}
